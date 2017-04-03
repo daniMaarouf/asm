12a41bb06c1a4181f5e5c19a7caf603fee35b4d6
daniMaarouf <dani@DANI-LAPTOP.localdomain>
2017-04-03 18:06:58 -0400

    Added to test program

--- div.asm
+++ div.asm
@@ -2,9 +2,11 @@
     rem $r1, $r0, 10
     out $r1             #should be 3
     load $r0, 45
+    out $r0             #should be 45
     div $r1, $r0, $r1
     out $r1             #should be 15
-    srl $r1             
+    srl $r1
+    out $r1             #should be 7
     mul $r1, $r1, 100
     out $r1             #should be 700
 
