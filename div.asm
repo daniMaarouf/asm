acef815ec879b512fe94367221b845c287a366da
daniMaarouf <dani@DANI-LAPTOP.localdomain>
2017-04-03 20:14:30 -0400

    Changed test file

--- div.asm
+++ div.asm
@@ -5,6 +5,8 @@
     out $r0             #should be 45
     div $r1, $r0, $r1
     out $r1             #should be 15
+    div $r1, $r0, 3
+    out $r1
     srl $r1
     out $r1             #should be 7
     mul $r1, $r1, 100
