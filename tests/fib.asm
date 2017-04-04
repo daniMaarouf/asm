5a9d9d2f3dbb1a9f8f820fb50a73f0dc87a15cd7
daniMaarouf <dani@DANI-LAPTOP.localdomain>
2017-04-04 18:29:59 -0400

    Working on binary coded decimal subroutine

--- tests/fib.asm
+++ tests/fib.asm
@@ -1,16 +1,30 @@
-        llo $r0, 1      #test comment
-        llo $r1, 1
-Loop2:
-        Loop1:   uadd $r2, $r1, $r0
-        uadd $r0, $r1, $zero
-        uadd $r1, $r2, $zero
-        llo $r6, 0xFF
-        lhi $r6, 0b01111111
-        sw $r6, $r0
-        lw $led, -0($r6)
-        llo $r7, -1
-        lhi $r7, 0
-        jmp $r7
-        llo $r7, -0b111
-        nop
-        
+    load $r0 0
+    load $r1 1
+    load $r2 0
+    load $r3 0
+loop: 
+    beq $r3, 10, end1
+    push $r0
+    call calculateBCD
+    pop $r0
+    out $r0
+    wait 500
+    uadd $r2, $r0, $r1
+    load $r0 $r1
+    load $r1 $r2
+    inc $r3
+    jmp loop
+end1:
+    out $r2
+    jmp end
+
+calculateBCD:
+    pop $r7         #return address
+    pop $r4         #argument
+    
+
+end2:
+    push $r4        #return value
+    push $r7        #return address
+    ret
+
---
+++ tests/old/fib.asm
@@ -0,0 +1,16 @@
+        llo $r0, 1      #test comment
+        llo $r1, 1
+Loop2:
+        Loop1:   uadd $r2, $r1, $r0
+        uadd $r0, $r1, $zero
+        uadd $r1, $r2, $zero
+        llo $r6, 0xFF
+        lhi $r6, 0b01111111
+        sw $r6, $r0
+        lw $led, -0($r6)
+        llo $r7, -1
+        lhi $r7, 0
+        jmp $r7
+        llo $r7, -0b111
+        nop
+        
