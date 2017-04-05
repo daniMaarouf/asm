8128461cb9b7d746515d078e96644f753c5ab05b
Dani Maarouf <dmaarouf@mail.uoguelph.ca>
2017-04-05 15:47:35 -0400

    Working on demo program

--- tests/demo.asm
+++ tests/demo.asm
@@ -37,3 +37,149 @@ end:
     out 0x0000      #default
     jmp input
 
+basicFib:
+    load $r0, 1
+    load $r1, 1
+basicFibInput:
+    out 0x3333
+    in $r3
+    and $r4, $r3, 0x80
+    bne $r4, $zero, basicFibInput
+    and $r3, $r3, 0x7F
+    clear $r2
+    clear $r4
+basicFibLoop:
+    beq $r4, $r3, basicFibEnd
+    uadd $r2, $r0, $r1
+    load $r0, $r1
+    load $r1, $r2
+    jmp basicFibLoop
+basicFibEnd:
+    push $r2
+    calculateBCD
+    pop $r2
+    out $r2
+    wait 1000
+    ret
+
+recursiveFib:
+    out 0x4444
+    in $r0
+    and $r1, $r0, 0x80
+    bne $r1, $zero, recursiveFib
+    and $r0, $r0, 0x7F
+    load $r1 0
+recursiveFibLoop:
+    bge $r1, $r0, recursiveFibEnd
+    push $r1
+    call calcFib
+    pop $r2
+    
+    push $r2
+    call calculateBCD
+    pop $r2
+
+    out $r2
+    inc $r1
+    wait 250
+    jmp recursiveFibLoop
+recursiveFibEnd:
+    out $r2
+    wait 1000
+    ret
+
+calcFib:
+    pop $r7     #return address now in r7
+    pop $r4     #argument now in $r4
+    bgt $r4, 1, recurse
+    push 1      #return value is 1
+    push $r7
+    ret
+recurse:
+    push $r7    #put return address back on stack
+    usub $r4, $r4, 1
+    push $r4            #first number to calc
+    usub $r4, $r4, 1
+    push $r4            #second number to calc
+    call calcFib
+    pop $r4     #pop new return value
+    pop $r5
+    push $r4    #stash the new return value for first
+    push $r5    #put second number to calc on top of stack
+    call calcFib
+    pop $r4     #second return val
+    pop $r5     #first return val
+    pop $r7     #return address
+    uadd $r4, $r4, $r5
+    push $r4
+    push $r7
+    ret
+
+exponentiation:
+    in $r0
+    and $r1, $r0, 0x80
+    bne $r1, $zero, exponentiation
+expLoop1:
+    in $r1
+    and $r2, $r1, 0x80
+    beq $r2, $zero, expLoop1
+    and $r0, $r0, 0x7F
+    and $r1, $r1, 0x7F
+
+    load $r2 1
+
+expLoop2:
+    beq $r1, $zero, expEnd
+    mul $r2, $r2, $r0
+    dec $r1
+    jmp expLoop2
+expEnd:
+    push $r2
+    call calculateBCD
+    pop $r2
+    out $r2
+    wait 1000
+    ret
+
+calculateBCD:
+    pop $r7
+    pop $r4
+    push $r7
+    
+    clear $r6
+    
+    rem $r5, $r4, 10
+    or $r6, $r6, $r5
+    
+    div $r5, $r4, 10
+    rem $r5, $r5, 10
+    clear $r7
+loop1:
+    sll $r5, $r5
+    inc $r7
+    bne $r7, 4, loop1
+    or $r6, $r6, $r5
+    
+    div $r5, $r4, 100
+    rem $r5, $r5, 10
+    clear $r7
+loop2:
+    sll $r5, $r5
+    inc $r7
+    bne $r7, 8, loop2
+    or $r6, $r6, $r5
+
+    div $r5, $r4, 1000
+    rem $r5, $r5, 10
+    clear $r7
+loop3:
+    sll $r5, $r5
+    inc $r7
+    bne $r7, 12, loop3
+    or $r6, $r6, $r5
+
+    pop $r7
+    push $r6
+    push $r7
+    ret
+
